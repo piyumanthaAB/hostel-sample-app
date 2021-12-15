ca7fb2a6be06b86660f1827dc82518e85b883a88
piyumanthaAB <cherinpiyumantha@gmail.com>
2021-12-15 13:45:00 +0530

    6th commit

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -82,10 +82,10 @@ const Navbar = (scrollToTop) => {
                <Links>
                     <Ul>
                         <Li className="li" onClick={scrollToTop}><Link to ="/">Home<Span className="Ho"></Span></Link></Li>
-                        <Li className="li" onClick={scrollToTop}><Link >Facilities<Span className="Ho"></Span></Link></Li>
+                        <Li className="li" onClick={scrollToTop}><Link to ="/">Facilities<Span className="Ho"></Span></Link></Li>
                         <Li className="li" onClick={scrollToTop}><Link to ="/rules">Rules & Regulations<Span className="Ho"></Span></Link></Li>
-                        <Li className="li" onClick={scrollToTop}><Link >Maintaince<Span className="Ho"></Span></Link></Li>
-                        <Li className="li" onClick={scrollToTop}><Link >Contact Us<Span className="Ho"></Span></Link></Li>
+                        <Li className="li" onClick={scrollToTop}><Link to ="/">Maintaince<Span className="Ho"></Span></Link></Li>
+                        <Li className="li" onClick={scrollToTop}><Link to ="/">Contact Us<Span className="Ho"></Span></Link></Li>
                     </Ul>
                 </Links>
             </Nav>
